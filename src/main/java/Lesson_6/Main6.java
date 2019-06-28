478b233a3b006f13002ce7e083d4a125c6df33dc
Feejie <46225295+Feejie@users.noreply.github.com>
2019-06-28 19:27:56 +0500

    Homework 6

--- src/main/java/Lesson_6/Main6.java
+++ src/main/java/Lesson_6/Main6.java
@@ -1,15 +1,41 @@
 package Lesson_6;
 
+import java.util.Random;
+
 public class Main6 {
 
     public static void main(String[] args) {
-        testTree();
+//        testTree();
 
-//        Tree<Integer> tree = new TreeImpl<>();
+        testBalancedTree(20,4, -20, 20);
+    }
+
+    private static void testBalancedTree(int quantity, int depth, int min, int max) {
+        Random random = new Random();
+        int balancedTrees = 0;
+
+        for (int i = 0; i < quantity; i++) {
+            Tree<Integer> tree = new TreeImpl<>(depth);
+
+            treeGenerator(depth, min, max, random, tree);
+
+            if (tree.isBalanced()) {
+                balancedTrees++;
+                tree.display();
+            }
+        }
+
+        System.out.println("Balanced Trees: " + ((balancedTrees / (quantity * 1.0)) * 100) + "%");
+    }
+
+    private static void treeGenerator(int depth, int min, int max, Random random, Tree<Integer> tree) {
+        for (int j = 0; j < (int)Math.pow(2, depth) - 1; j++) {
+            tree.add(random.nextInt((max - min) + 1) + min);
+        }
     }
 
     private static void testTree() {
-        Tree<Integer> tree = new TreeImpl<>();
+        Tree<Integer> tree = new TreeImpl<>(6);
         tree.add(60);
         tree.add(50);
         tree.add(66);
--- src/main/java/Lesson_6/TreeImpl.java
+++ src/main/java/Lesson_6/TreeImpl.java
@@ -7,6 +7,12 @@ public class TreeImpl<E extends Comparable<? super E>> implements Tree<E> {
     private Node<E> root;
     private int size;
 
+    private int maxLevel;
+
+    public TreeImpl(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
     @Override
     public boolean add(E value) {
         Node<E> newNode = new Node<>(value);
@@ -17,6 +23,11 @@ public class TreeImpl<E extends Comparable<? super E>> implements Tree<E> {
 
         NodeAndParent nodeAndParent = doFind(value);
         if (nodeAndParent.current != null) {
+            return false;
+        }
+
+        int parentLevel = nodeAndParent.parent.getLevel();   // !!
+        if (parentLevel > maxLevel - 1) {
             return false;
         }
 
@@ -42,12 +53,17 @@ public class TreeImpl<E extends Comparable<? super E>> implements Tree<E> {
         Node<E> parent = null;
         Node<E> current = this.root;
 
+        int nodeLevel = 0;
+
         while (current != null) {
             if (current.getValue().equals(value)) {
+                current.setLevel(++nodeLevel);   // !!
                 return new NodeAndParent(current, parent);
             }
 
             parent = current;
+            parent.setLevel(++nodeLevel);
+
             if (current.shouldBeLeft(value)) {
                 current = current.getLeftChild();
             } else {
@@ -248,6 +264,22 @@ public class TreeImpl<E extends Comparable<? super E>> implements Tree<E> {
             nBlanks /= 2;
         }
         System.out.println("................................................................");
+    }
+
+    @Override
+    public boolean isBalanced() {
+        return isBalanced(root);
+    }
+
+    private boolean isBalanced(Node node) {
+        return (node == null) ||
+                isBalanced(node.getLeftChild()) &&
+                        isBalanced(node.getRightChild()) &&
+                        Math.abs(height(node.getLeftChild()) - height(node.getRightChild())) <= 1;
+    }
+
+    private int height(Node node) {
+        return node == null ? 0 : 1 + Math.max(height(node.getLeftChild()), height(node.getRightChild()));
     }
 
     private class NodeAndParent {
